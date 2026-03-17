e02eefea6503953cd68f3056177b92efc46fbb3e
l3rit2026 <isesodl3rit2026@gmail.com>
2026-03-17 12:49:28 +0100

    creation BOUTTON ENREGISTER avec l'instruction try catch par CIZA RWABASHI RICHARD

--- Td_VbNET/Td_VbNET/Form1.Designer.vb
+++ Td_VbNET/Td_VbNET/Form1.Designer.vb
@@ -24,33 +24,49 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.BntModifier = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 76)
+        Me.Label1.Location = New System.Drawing.Point(9, 62)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(80, 17)
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Label kevin"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 130)
+        Me.Label2.Location = New System.Drawing.Point(9, 106)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(80, 17)
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Label kevin"
         '
+        'BntModifier
+        '
+        Me.BntModifier.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BntModifier.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BntModifier.Location = New System.Drawing.Point(26, 273)
+        Me.BntModifier.Name = "BntModifier"
+        Me.BntModifier.Size = New System.Drawing.Size(110, 35)
+        Me.BntModifier.TabIndex = 2
+        Me.BntModifier.Text = "MODIFIER "
+        Me.BntModifier.UseVisualStyleBackColor = False
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.ClientSize = New System.Drawing.Size(523, 331)
+        Me.Controls.Add(Me.BntModifier)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -59,5 +75,6 @@ Partial Class Form1
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents BntModifier As System.Windows.Forms.Button
 
 End Class
--- Td_VbNET/Td_VbNET/Form1.vb
+++ Td_VbNET/Td_VbNET/Form1.vb
@@ -7,4 +7,13 @@
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BntModifier.Click
+        Try
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
 End Class
