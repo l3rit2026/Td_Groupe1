00fad9eb25377e30f5c15bbd53875b6f507a61d4
l3rit2026 <isesodl3rit2026@gmail.com>
2026-03-17 14:20:36 +0200

    ajouter un bouton qui calcule la somme de deux nombre saisir faite par KYAKASWA BONGWA YANNICK

--- Td_VbNET/Td_VbNET/Form1.Designer.vb
+++ Td_VbNET/Td_VbNET/Form1.Designer.vb
@@ -25,25 +25,26 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.BntModifier = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.nb1 = New System.Windows.Forms.TextBox()
+        Me.nb2 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(9, 62)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Location = New System.Drawing.Point(12, 76)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.Size = New System.Drawing.Size(80, 17)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Label kevin"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(9, 106)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Location = New System.Drawing.Point(12, 130)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.Size = New System.Drawing.Size(80, 17)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Label kevin"
         '
@@ -51,22 +52,49 @@ Partial Class Form1
         '
         Me.BntModifier.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.BntModifier.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.BntModifier.Location = New System.Drawing.Point(26, 273)
+        Me.BntModifier.Location = New System.Drawing.Point(35, 336)
+        Me.BntModifier.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BntModifier.Name = "BntModifier"
-        Me.BntModifier.Size = New System.Drawing.Size(110, 35)
+        Me.BntModifier.Size = New System.Drawing.Size(147, 43)
         Me.BntModifier.TabIndex = 2
         Me.BntModifier.Text = "MODIFIER "
         Me.BntModifier.UseVisualStyleBackColor = False
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(203, 336)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(138, 43)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "somme deux nombre"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'nb1
+        '
+        Me.nb1.Location = New System.Drawing.Point(203, 308)
+        Me.nb1.Name = "nb1"
+        Me.nb1.Size = New System.Drawing.Size(45, 22)
+        Me.nb1.TabIndex = 4
+        '
+        'nb2
+        '
+        Me.nb2.Location = New System.Drawing.Point(287, 308)
+        Me.nb2.Name = "nb2"
+        Me.nb2.Size = New System.Drawing.Size(45, 22)
+        Me.nb2.TabIndex = 5
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(523, 331)
+        Me.ClientSize = New System.Drawing.Size(697, 407)
+        Me.Controls.Add(Me.nb2)
+        Me.Controls.Add(Me.nb1)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.BntModifier)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -76,5 +104,8 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents BntModifier As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents nb1 As System.Windows.Forms.TextBox
+    Friend WithEvents nb2 As System.Windows.Forms.TextBox
 
 End Class
--- Td_VbNET/Td_VbNET/Form1.vb
+++ Td_VbNET/Td_VbNET/Form1.vb
@@ -16,4 +16,16 @@
         End Try
 
     End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nombre1 As Double
+        Dim nombre2 As Double
+        Dim resultat As Double
+
+        nombre1 = CDbl(nb1.Text)
+        nombre2 = CDbl(nb2.Text)
+        resultat = nombre1 + nombre2
+
+        MessageBox.Show("La somme est : " & resultat)
+    End Sub
 End Class
